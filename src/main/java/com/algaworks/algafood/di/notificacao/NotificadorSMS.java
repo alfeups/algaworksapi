8b44b6ca93ddf8a6ed17372f6ba6d9327693e238
alfeups <alfeups@hotmail.com>
2022-03-20 00:34:45 -0300

    my first commit

---
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorSMS.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.di.notificacao;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+
+//@Primary
+//@Qualifier("normal")
+
+//@Profile("prod")
+@TipoDoNotificador(NivelUrgencia.URGENTE)
+@Component
+public class NotificadorSMS implements Notificador{
+
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("Notificando %s por SMS através do telefone %s: %s\n",
+				cliente.getNome(), cliente.getTelefone(), mensagem);
+	}
+}
