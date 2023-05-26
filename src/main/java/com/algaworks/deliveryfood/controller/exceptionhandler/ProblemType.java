a8630b60e19f888683aa148f76b335f5537f6876
alfeups <alfeups@hotmail.com>
2023-05-25 22:17:19 -0300

    adding handler when resource not found

--- src/main/java/com/algaworks/deliveryfood/controller/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/deliveryfood/controller/exceptionhandler/ProblemType.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum ProblemType {
 
     NAO_FOI_POSSIVEL_LER_BODY("/nao-foi-possivel-ler-body", "Não foi possível ler body da requisição."),
-    ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada."),
+    RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado."),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso."),
     NEGOCIO_EXPCETION("/negocio-exception", "Violação de regra de degócio."),
     PARAMETRO_INVALIDO("/parametro-invalido", "Paramêtro inválido.");
